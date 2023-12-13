1a4e3940ecf1b3f49855dd93b425f684a360f708
ibraim alibay <ibraim.alibay@gmail.com>
2023-12-13 00:25:39 -0500

    feat: add goroutine

---
+++ goroutine/errors.go
@@ -0,0 +1,17 @@
+package goroutine
+
+import (
+	"context"
+
+	kit "github.com/Alibay/go-kit"
+)
+
+const (
+	ErrCodeGoroutineNoLogger = "GORTN-001"
+)
+
+var (
+	ErrGoroutineNoLogger = func(ctx context.Context) error {
+		return kit.NewAppErrBuilder(ErrCodeGoroutineNoLogger, "either logger or logger func must be specified").C(ctx).Err()
+	}
+)
