9ff7793d757c5c7922357e89706b6329233a31a2
ibraim alibay <ibraim.alibay@gmail.com>
2023-12-13 14:29:54 -0500

    feat: add monitoring

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/pkg/errors v0.9.1
 	github.com/pressly/goose/v3 v3.15.0
+	github.com/prometheus/client_golang v1.17.0
 	github.com/rs/cors v1.10.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.9.3
@@ -51,12 +52,14 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.5 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.26.5 // indirect
 	github.com/aws/smithy-go v1.19.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.6.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.4.0 // indirect
@@ -71,11 +74,15 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/paulmach/orb v0.10.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.18 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
@@ -98,6 +105,7 @@ require (
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ monitoring/prometheus.go
@@ -0,0 +1,101 @@
+package monitoring
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"regexp"
+
+	"github.com/Alibay/go-kit/logger"
+
+	kit "github.com/Alibay/go-kit"
+	"github.com/Alibay/go-kit/goroutine"
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type prometheusMetricsSrv struct {
+	log        logger.CLoggerFunc
+	registerer *prometheus.Registry
+	router     *mux.Router
+	httpSrv    *http.Server
+}
+
+func NewMetricsServer(logger logger.CLoggerFunc) MetricsServer {
+	srv := &prometheusMetricsSrv{
+		log: logger,
+	}
+	return srv
+}
+
+func (s *prometheusMetricsSrv) l() logger.CLogger {
+	return s.log().Pr("http").Cmp("prometheus")
+}
+
+func (s *prometheusMetricsSrv) Init(config *Config, metricProviders ...MetricsProvider) error {
+
+	if match, _ := regexp.MatchString("^\\d{1,6}$", config.Port); !match {
+		return ErrPrometheusInvalidPort(config.Port)
+	}
+
+	url := config.UrlPath
+	if url == "" {
+		url = "/metrics"
+	}
+
+	s.registerer = prometheus.NewRegistry()
+
+	for _, pr := range metricProviders {
+		for _, m := range pr.GetCollector()() {
+			if err := s.registerer.Register(m); err != nil {
+				return ErrPrometheusRegisterAppMetrics(err)
+			}
+		}
+	}
+
+	if config.GoMetrics {
+		if err := s.registerer.Register(collectors.NewGoCollector()); err != nil {
+			return ErrPrometheusRegisterGoMetrics(err)
+		}
+		if err := s.registerer.Register(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{})); err != nil {
+			return ErrPrometheusRegisterProcessMetrics(err)
+		}
+	}
+
+	s.router = mux.NewRouter()
+	s.router.Path(url).Handler(promhttp.HandlerFor(s.registerer, promhttp.HandlerOpts{}))
+
+	s.httpSrv = &http.Server{
+		Addr:    fmt.Sprintf(":%s", config.Port),
+		Handler: s.router,
+	}
+
+	return nil
+
+}
+
+func (s *prometheusMetricsSrv) Listen() {
+	goroutine.New().
+		WithLoggerFn(s.log).
+		WithRetry(goroutine.Unrestricted).
+		Go(context.Background(),
+			func() {
+				l := s.l().Mth("listen").F(kit.KV{"url": s.httpSrv.Addr}).Inf("listening")
+				if err := s.httpSrv.ListenAndServe(); err != nil {
+					if !errors.Is(err, http.ErrServerClosed) {
+						l.E(ErrPrometheusHttpServer(err)).St().Err()
+					} else {
+						l.Dbg("server closed")
+					}
+				}
+			},
+		)
+}
+
+func (s *prometheusMetricsSrv) Close() {
+	_ = s.httpSrv.Close()
+	s.l().Inf("closed")
+}
